f7283e953b4e6087e730f31c13428d855f7fb473
buzhuyu <buzhuyu@huawei.com>
2022-07-13 18:54:29 +0800

    Add inspector describion:add inspector issue:https://gitee.com/openharmony-sig/arkcompiler_toolchain/issues/I5GYFK
    Signed-off-by: buzhuyu <buzhuyu@huawei.com>

---
+++ inspector/ws_server.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "inspector/ws_server.h"
+
+#include <fstream>
+#include <iostream>
+#include <sys/types.h>
+
+#include "base/log/log.h"
+
+namespace OHOS::ArkCompiler::Toolchain {
+void WsServer::RunServer()
+{
+    terminateExecution_ = false;
+    try {
+        ioContext_ = std::make_unique<boost::asio::io_context>();
+        int appPid = getpid();
+        tid_ = pthread_self();
+        std::string pidStr = std::to_string(appPid);
+        std::string instanceIdStr("");
+        auto& connectFlag = connectState_;
+        /**
+         * The old version of IDE is not compatible with the new images due to the connect server.
+         * The First instance will use "pid" instead of "pid + instanceId" to avoid this.
+         * If old version of IDE does not get the instanceId by connect server, it can still connect the debug server.
+         */
+        if (instanceId_ != 0) {
+            instanceIdStr = std::to_string(instanceId_);
+        }
+        std::string sockName = '\0' + pidStr + instanceIdStr + componentName_;
+        LOGI("WsServer RunServer: %{public}d%{public}d%{public}s", appPid, instanceId_, componentName_.c_str());
+        localSocket::endpoint endPoint(sockName);
+        localSocket::socket socket(*ioContext_);
+        localSocket::acceptor acceptor(*ioContext_, endPoint);
+        acceptor.async_accept(socket, [&connectFlag](const boost::system::error_code& error) {
+            if (!error) {
+                connectFlag = true;
+            }
+        });
+        ioContext_->run();
+        if (terminateExecution_ || !connectState_) {
+            return;
+        }
+        webSocket_ = std::unique_ptr<websocket::stream<localSocket::socket>>(
+            std::make_unique<websocket::stream<localSocket::socket>>(std::move(socket)));
+        webSocket_->accept();
+        while (!terminateExecution_) {
+            beast::flat_buffer buffer;
+            boost::system::error_code error;
+            webSocket_->read(buffer, error);
+            if (error) {
+                webSocket_.reset();
+                return;
+            }
+            std::string message = boost::beast::buffers_to_string(buffer.data());
+            LOGI("WsServer OnMessage: %{public}s", message.c_str());
+            wsOnMessage_(std::move(message));
+        }
+    } catch (const beast::system_error& se) {
+        if (se.code() != websocket::error::closed) {
+            webSocket_.reset();
+            LOGE("Error system_error");
+        }
+    } catch (const std::exception& e) {
+        LOGE("Error exception, %{public}s", e.what());
+    }
+}
+
+void WsServer::StopServer()
+{
+    LOGI("WsServer StopServer");
+    terminateExecution_ = true;
+    if (!connectState_) {
+        ioContext_->stop();
+    } else {
+        boost::system::error_code error;
+        webSocket_->close(websocket::close_code::normal, error);
+    }
+    pthread_join(tid_, NULL);
+}
+
+void WsServer::SendReply(const std::string& message) const
+{
+    LOGI("WsServer SendReply: %{public}s", message.c_str());
+    try {
+        boost::beast::multi_buffer buffer;
+        boost::beast::ostream(buffer) << message;
+
+        webSocket_->text(webSocket_->got_text());
+        webSocket_->write(buffer.data());
+    } catch (const beast::system_error& se) {
+        if (se.code() != websocket::error::closed) {
+            LOGE("SendReply Error system_error");
+        }
+    } catch (const std::exception& e) {
+        LOGE("SendReply Error exception");
+    }
+}
+} // namespace OHOS::ArkCompiler::Toolchain
---
+++ inspector/ws_server.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ARKCOMPILER_TOOLCHAIN_INSPECTOR_WS_SERVER_H
+#define ARKCOMPILER_TOOLCHAIN_INSPECTOR_WS_SERVER_H
+
+#include <functional>
+#include <iostream>
+#include <queue>
+
+#include <boost/asio/error.hpp>
+#include <boost/asio/local/stream_protocol.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+
+namespace OHOS::ArkCompiler::Toolchain {
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+using localSocket = boost::asio::local::stream_protocol;
+
+class WsServer {
+public:
+    WsServer(const std::string& component, const std::function<void(std::string&&)>& onMessage, int32_t instanceId)
+        : instanceId_(instanceId), componentName_(component), wsOnMessage_(onMessage)
+    {}
+    ~WsServer() = default;
+    void RunServer();
+    void StopServer();
+    void SendReply(const std::string& message) const;
+
+private:
+    std::atomic<bool> connectState_ {false};
+    std::atomic<bool> terminateExecution_ { false };
+    int32_t instanceId_ {0};
+    pthread_t tid_ {0};
+    std::string componentName_ {};
+    std::function<void(std::string&&)> wsOnMessage_ {};
+    std::unique_ptr<websocket::stream<localSocket::socket>> webSocket_ { nullptr };
+    std::unique_ptr<boost::asio::io_context> ioContext_ { nullptr };
+};
+} // namespace OHOS::ArkCompiler::Toolchain
+
+#endif // ARKCOMPILER_TOOLCHAIN_INSPECTOR_WS_SERVER_H
