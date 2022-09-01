8c88c7e04dd48106b89394b9aa45f8574842b5f1
huangfeijie <huangfeijie@huawei.com>
2022-09-01 11:33:05 +0800

    issue: https://gitee.com/openharmony/arkcompiler_toolchain/issues/I5P5GX
    Replace the boost websocket
    
    Implement a new websocket to replace the boost
    
    Signed-off-by: huangfeijie <huangfeijie@huawei.com>

---
+++ websocket/websocket.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ARKCOMPILER_TOOLCHAIN_WEBSOCKET_WEBSOCKET_H
+#define ARKCOMPILER_TOOLCHAIN_WEBSOCKET_WEBSOCKET_H
+
+#include <iostream>
+#include <optional>
+
+namespace OHOS::ArkCompiler::Toolchain {
+struct WebSocketFrame {
+    uint8_t fin;
+    uint8_t opcode;
+    uint8_t mask;
+    uint64_t payloadLen;
+    char maskingkey[5];
+    std::unique_ptr<char []> payload;
+};
+
+struct HttpProtocol {
+    std::string connection;
+    std::string upgrade;
+    std::string version;
+    std::string secWebSocketKey;
+};
+
+class WebSocket {
+public:
+    WebSocket() = default;
+    ~WebSocket() = default;
+    std::optional<std::string> Decode();
+    void Close();
+    bool DecodeMessage(WebSocketFrame& wsFrame);
+    bool HttpHandShake();
+    bool HttpProtocolDecode(const std::string& request, HttpProtocol& req);
+    bool HandleFrame(WebSocketFrame& wsFrame);
+    bool ProtocolUpgrade(const HttpProtocol& req);
+    void SendReply(const std::string& message) const;
+    bool StartForSimulator();
+    bool StartWebSocket(std::string sockName);
+    std::atomic<bool> connectState_;
+
+private:
+    int32_t client_ {0};
+    int32_t fd_ {0};
+    const int32_t ENCODED_KEY_LEN = 128;
+    const char* EOL = "\r\n";
+    const int32_t SOCKET_HANDSHAKE_LEN = 1024;
+    const int32_t SOCKET_HEADER_LEN = 2;
+    const int32_t SOCKET_MASK_LEN = 4;
+    const int32_t SOCKET_SUCCESS = 0;
+    const int32_t PAYLOAD_LEN = 2;
+    const int32_t EXTEND_PATLOAD_LEN = 8;
+    const char* WEB_SOCKET_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+};
+} // namespace OHOS::ArkCompiler::Toolchain
+
+#endif // ARKCOMPILER_TOOLCHAIN_WEBSOCKET_WEBSOCKET_H
