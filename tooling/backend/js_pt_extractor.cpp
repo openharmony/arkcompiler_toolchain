64fafc1124cdfd4dbe547ad7c5dfa2abb66e5053
Gymee <yumeijie@huawei.com>
2022-09-21 17:07:22 +0800

    add tooling
    Issue: #I5RV8U
    Signed-off-by: Gymee <yumeijie@huawei.com>
    Change-Id: Id2bee91fb49f91dbc683766c19551725b0076299

---
+++ tooling/backend/js_pt_extractor.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ecmascript/tooling/backend/js_pt_extractor.h"
+
+#include "ecmascript/tooling/backend/debugger_api.h"
+
+namespace panda::ecmascript::tooling {
+uint32_t JSPtExtractor::SingleStepper::GetStackDepth() const
+{
+    return DebuggerApi::GetStackDepth(ecmaVm_);
+}
+
+bool JSPtExtractor::SingleStepper::InStepRange(uint32_t pc) const
+{
+    for (const auto &range : stepRanges_) {
+        if (pc >= range.startBcOffset && pc < range.endBcOffset) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool JSPtExtractor::SingleStepper::StepComplete(uint32_t bcOffset) const
+{
+    std::unique_ptr<PtMethod> ptMethod = DebuggerApi::GetMethod(ecmaVm_);
+    uint32_t stackDepth = GetStackDepth();
+
+    switch (type_) {
+        case Type::STEP_INTO: {
+            if ((method_->GetMethodId() == ptMethod->GetMethodId()) &&
+                (method_->GetJSPandaFile() == ptMethod->GetJSPandaFile()) &&
+                (InStepRange(bcOffset))) {
+                return false;
+            }
+            break;
+        }
+        case Type::STEP_OVER: {
+            if (stackDepth_ < stackDepth) {
+                return false;
+            }
+            if (stackDepth_ == stackDepth && InStepRange(bcOffset)) {
+                return false;
+            }
+            break;
+        }
+        case Type::STEP_OUT: {
+            if (stackDepth_ <= stackDepth) {
+                return false;
+            }
+            break;
+        }
+        default: {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::unique_ptr<JSPtExtractor::SingleStepper> JSPtExtractor::GetStepIntoStepper(const EcmaVM *ecmaVm)
+{
+    return GetStepper(ecmaVm, SingleStepper::Type::STEP_INTO);
+}
+
+std::unique_ptr<JSPtExtractor::SingleStepper> JSPtExtractor::GetStepOverStepper(const EcmaVM *ecmaVm)
+{
+    return GetStepper(ecmaVm, SingleStepper::Type::STEP_OVER);
+}
+
+std::unique_ptr<JSPtExtractor::SingleStepper> JSPtExtractor::GetStepOutStepper(const EcmaVM *ecmaVm)
+{
+    return GetStepper(ecmaVm, SingleStepper::Type::STEP_OUT);
+}
+
+std::list<JSPtStepRange> JSPtExtractor::GetStepRanges(File::EntityId methodId, uint32_t offset)
+{
+    std::list<JSPtStepRange> ranges {};
+    const LineNumberTable &table = GetLineNumberTable(methodId);
+    auto callbackFunc = [table, &ranges](int32_t line) -> bool {
+        for (auto it = table.begin(); it != table.end(); ++it) {
+            auto next = it + 1;
+            if (it->line == line) {
+                JSPtStepRange range {it->offset, next != table.end() ? next->offset : UINT32_MAX};
+                ranges.push_back(range);
+            }
+        }
+        return true;
+    };
+    MatchLineWithOffset(callbackFunc, methodId, offset);
+    return ranges;
+}
+
+std::unique_ptr<JSPtExtractor::SingleStepper> JSPtExtractor::GetStepper(const EcmaVM *ecmaVm, SingleStepper::Type type)
+{
+    std::unique_ptr<PtMethod> ptMethod = DebuggerApi::GetMethod(ecmaVm);
+    ASSERT(ptMethod != nullptr);
+
+    if (type == SingleStepper::Type::STEP_OUT) {
+        return std::make_unique<SingleStepper>(ecmaVm, std::move(ptMethod), std::list<JSPtStepRange> {}, type);
+    }
+
+    std::list<JSPtStepRange> ranges = GetStepRanges(ptMethod->GetMethodId(), DebuggerApi::GetBytecodeOffset(ecmaVm));
+    return std::make_unique<SingleStepper>(ecmaVm, std::move(ptMethod), std::move(ranges), type);
+}
+}  // namespace panda::ecmascript::tooling
---
+++ tooling/debugger_service.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ECMASCRIPT_TOOLING_DEBUGGER_SERVICE_H
+#define ECMASCRIPT_TOOLING_DEBUGGER_SERVICE_H
+
+#include <functional>
+#include <string>
+
+#include "ecmascript/common.h"
+
+namespace panda::ecmascript {
+class EcmaVM;
+}  // namespace panda::ecmascript
+
+namespace panda::ecmascript::tooling {
+#ifdef __cplusplus
+#if __cplusplus
+extern "C" {
+#endif
+#endif /* End of #ifdef __cplusplus */
+
+PUBLIC_API void InitializeDebugger(::panda::ecmascript::EcmaVM *vm,
+                                   const std::function<void(const void *, const std::string &)> &onResponse);
+
+PUBLIC_API void UninitializeDebugger(::panda::ecmascript::EcmaVM *vm);
+
+PUBLIC_API void OnMessage(const ::panda::ecmascript::EcmaVM *vm, std::string &&message);
+
+PUBLIC_API void WaitForDebugger(const ::panda::ecmascript::EcmaVM *vm);
+
+PUBLIC_API void ProcessMessage(const ::panda::ecmascript::EcmaVM *vm);
+
+PUBLIC_API int32_t GetDispatchStatus(const ::panda::ecmascript::EcmaVM *vm);
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* End of #ifdef __cplusplus */
+}  // panda::ecmascript::tooling
+
+#endif
