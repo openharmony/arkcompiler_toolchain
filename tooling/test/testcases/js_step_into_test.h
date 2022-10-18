fa17b005a0288bd71f7adbf15b1ecbd72e400042
ctw-ian <chentingwei2@huawei.com>
2022-10-18 16:15:28 +0800

    Fix a step into test case
    Issue:I5WC0Z
    
    Signed-off-by: ctw-ian <chentingwei2@huawei.com>
    Change-Id: I1f3d50d7fbd0c0dfe709d04facc7746b56472b2a

--- tooling/test/testcases/js_step_into_test.h
+++ tooling/test/testcases/js_step_into_test.h
@@ -34,9 +34,8 @@ public:
             size_t breakpoint[POINTER_SIZE][LINE_COLUMN] =
                 {{84, 0}, {87, 0}, {27, 0}, {79, 0}, {42, 0}, {38, 0}, {56, 0}, {60, 0}, {96, 0}, {54, 0}};
             // line number for stepinto array
-            // 35 -> 36; 49 -> 50
             size_t stepInto[STEP_SIZE][LINE_COLUMN] =
-                {{85, 5}, {23, 0}, {73, 0}, {80, 0}, {35, 0}, {43, 0}, {49, 0}, {61, 0}, {97, 15}};
+                {{85, 5}, {23, 0}, {73, 0}, {80, 0}, {36, 0}, {43, 0}, {50, 0}, {61, 0}, {97, 15}};
             SetJSPtLocation(breakpoint[0], POINTER_SIZE, pointerLocations_);
             SetJSPtLocation(stepInto[0], STEP_SIZE, stepLocations_);
             TestUtil::SuspendUntilContinue(DebugEvent::LOAD_MODULE);
