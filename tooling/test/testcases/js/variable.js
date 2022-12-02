25cf981edf32bb425ebcb3fe1808f21e19dd7538
zhouwenxuan <zhouwenxuan1@huawei.com>
2022-12-02 16:10:36 +0800

    Add tooling UT testcase
    issue: https://gitee.com/openharmony/arkcompiler_toolchain/issues/I64CI6
    
    Signed-off-by: zhouwenxuan <zhouwenxuan1@huawei.com>

--- tooling/test/testcases/js/variable.js
+++ tooling/test/testcases/js/variable.js
@@ -37,10 +37,14 @@ var o = {
             yield 3;
         }
         var regexp0 = /^\d+\.\d+$/i;
-        var arraybuffer0 = new ArrayBuffer(10);
+        var arraybuffer0 = new ArrayBuffer(24);
         var uint8array0 = new Uint8Array(arraybuffer0);
         const dataview0 = new DataView(arraybuffer0, 0);
         var bigint0 = BigInt(999n);
+        var typedarray0 = new Uint8Array();
+        var sharedarraybuffer0 = new SharedArrayBuffer(32);
+        var iterator0 = string0[Symbol.iterator];
+        var weakref0 = new WeakRef(obj0);
 
         var set1 = new Set();
         set1.add(number0);
@@ -262,6 +266,65 @@ var o = {
         var regExp19 = new RegExp('^[1-9]*[1-9][0-9]*$', 'm');
         var regExp20 = /^[a-zA-Z]\/w{5,17}$/;
         var regExp21 = new RegExp('^[0-9a-zA-Z_]{1,}$', 'u');
+
+        var array1 = [];
+        array1.push('banana', 'apple', 'peach');
+        var array2 = ['banana', 'apple'];
+        array2.push('peach');
+        var array3 = ['banana', 'apple', 'peach'];
+        array3.pop();
+        var array4 = array3.shift();
+        var array5 = new Array(number0);
+        var array6 = new Array(string0);
+        var array7 = new Array(boolean0);
+        var array8 = new Array(obj0);
+        var array9 = new Array(function0);
+        var array10 = new Array(map0);
+        var array11 = new Array(set0);
+        var array12 = new Array(undefined0);
+        var array13 = new Array(array0);
+        var array14 = new Array(generator0);
+        var array15 = new Array(regexp0);
+        var array16 = new Array(arraybuffer0);
+        var array17 = new Array(uint8array0);
+        var array18 = new Array(dataview0);
+        var array19 = new Array(bigint0);
+        var array20 = array1;
+        var array21 = array4;
+
+        var typedarray1 = new Int8Array();
+        var typedarray2 = new Uint8ClampedArray();
+        var typedarray3 = new Int16Array();
+        var typedarray4 = new Uint16Array();
+        var typedarray5 = new Int32Array();
+        var typedarray6 = new Uint32Array();
+        var typedarray7 = new Float32Array();
+        var typedarray8 = new Float64Array();
+        var typedarray9 = new BigInt64Array();
+        var typedarray10 = new BigUint64Array();
+        var typedarray11 = new Uint8Array(number0);
+
+        var iterator1 = array0[Symbol.iterator];
+        var iterator2 = map0[Symbol.iterator];
+        var iterator3 = set0[Symbol.iterator];
+        var iterator4 = uint8array0[Symbol.iterator];
+        var iterator5 = iterator0;
+        var iterator6 = typedarray0[Symbol.iterator];
+        var iterator7 = typedarray1[Symbol.iterator];
+        var iterator8 = typedarray2[Symbol.iterator];
+        var iterator9 = typedarray3[Symbol.iterator];
+        var iterator10 = typedarray4[Symbol.iterator];
+        var iterator11 = typedarray5[Symbol.iterator];
+        var iterator12 = typedarray6[Symbol.iterator];
+        var iterator13 = typedarray7[Symbol.iterator];
+        var iterator14 = typedarray8[Symbol.iterator];
+        var iterator15 = typedarray9[Symbol.iterator];
+        var iterator16 = typedarray10[Symbol.iterator];
+        var iterator17 = obj0[Symbol.iterator];
+        var iterator19 = sharedarraybuffer0[Symbol.iterator];
+
+        var weakMap = new WeakMap();
+        var weakSet = new WeakSet();
 
         var nop = undefined;
     }
