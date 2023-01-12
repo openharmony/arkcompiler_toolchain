70701ec5681f2fcd2e8edcff181bd209ef0721f1
cwx1148594 <caolili14@huawei.com>
2023-01-12 11:32:49 +0800

    Descriptor: heapprofiler support GC details: heapprofiler support GC issue:https://gitee.com/openharmony/xts_acts/issues/I6A4AG
    Signed-off-by: cwx1148594 <caolili14@huawei.com>

--- tooling/agent/heapprofiler_impl.cpp
+++ tooling/agent/heapprofiler_impl.cpp
@@ -263,6 +263,7 @@ DispatchResponse HeapProfilerImpl::AddInspectedHeapObject([[maybe_unused]] const
 DispatchResponse HeapProfilerImpl::CollectGarbage()
 {
     panda::JSNApi::TriggerGC(vm_, panda::JSNApi::TRIGGER_GC_TYPE::FULL_GC);
+    panda::JSNApi::TriggerGC(vm_, panda::JSNApi::TRIGGER_GC_TYPE::OLD_GC);
     return DispatchResponse::Ok();
 }
 
