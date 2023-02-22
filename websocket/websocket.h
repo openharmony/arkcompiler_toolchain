89560fb922c6a407393dc5a05449cc5c4e669ef6
huangfeijie <huangfeijie@huawei.com>
2023-02-22 15:48:25 +0800

    make adaptation to the layoutInspector Replaces static allocations issue: https://gitee.com/openharmony/arkcompiler_toolchain/issues/I6GYXF
    Signed-off-by: huangfeijie <huangfeijie@huawei.com>

--- websocket/websocket.h
+++ websocket/websocket.h
@@ -84,7 +84,7 @@ private:
     static constexpr int32_t SOCKET_MASK_LEN = 4;
     static constexpr int32_t SOCKET_SUCCESS = 0;
     static constexpr int32_t PAYLOAD_LEN = 2;
-    static constexpr int32_t EXTEND_PATLOAD_LEN = 8;
+    static constexpr int32_t EXTEND_PAYLOAD_LEN = 8;
     static constexpr char WEB_SOCKET_GUID[] = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 };
 } // namespace OHOS::ArkCompiler::Toolchain
