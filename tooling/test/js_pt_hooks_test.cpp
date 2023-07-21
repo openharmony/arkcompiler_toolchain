9c79ab90de0234e7f611631c8b395a73fbf58284
yang-19970325 <yangyang585@huawei.com>
2023-07-21 11:17:52 +0800

    fixed efb05b0 from https://gitee.com/yang-19970325/arkcompiler_toolchain/pulls/327 Fix router exception while debugger
    Issue；#I7MVMA
    
    Signed-off-by: yang-19970325 <yangyang585@huawei.com>
    Change-Id: Ida7e9ad23428d2669256ceecf59f847032236c24

--- tooling/test/js_pt_hooks_test.cpp
+++ tooling/test/js_pt_hooks_test.cpp
@@ -114,14 +114,6 @@ HWTEST_F_L0(JSPtHooksTest, VmDeathTest)
     ASSERT_NE(jspthooks, nullptr);
 }
 
-HWTEST_F_L0(JSPtHooksTest, PendingJobEntryTest)
-{
-    auto debugger = std::make_unique<DebuggerImpl>(ecmaVm, nullptr, nullptr);
-    std::unique_ptr<JSPtHooks> jspthooks = std::make_unique<JSPtHooks>(debugger.get());
-    jspthooks->PendingJobEntry();
-    ASSERT_NE(jspthooks, nullptr);
-}
-
 HWTEST_F_L0(JSPtHooksTest, NativeCallingTest)
 {
     auto debugger = std::make_unique<DebuggerImpl>(ecmaVm, nullptr, nullptr);
