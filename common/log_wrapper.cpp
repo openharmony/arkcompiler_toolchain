efc54cbb7d86cb97b8ef68618e0a04e1b0139546
liu-zelin <liuzelin8@huawei.com>
2023-11-13 10:35:30 +0800

    fix: build error
    Signed-off-by: liu-zelin <liuzelin8@huawei.com>
    Change-Id: I0f52a00dbee23f47267a60911f0b06e48beaa51d

--- common/log_wrapper.cpp
+++ common/log_wrapper.cpp
@@ -46,7 +46,10 @@ void StdLog::PrintLog(LogLevel level, const char* fmt, ...)
     std::string formatted = StripFormatString(fmt);
     va_list args;
     va_start(args, fmt);
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wformat-nonliteral"
     __android_log_vprint(static_cast<int>(level), tag, formatted.c_str(), args);
+#pragma clang diagnostic pop
     va_end(args);
 }
 #else
