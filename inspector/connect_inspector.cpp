41dcdb5f7584d7c01df94b2a50d4230fd174e561
fangting <fangting12@huawei.com>
2024-02-21 11:05:47 +0800

    fix thread name display error
    Signed-off-by: fangting <fangting12@huawei.com>

--- inspector/connect_inspector.cpp
+++ inspector/connect_inspector.cpp
@@ -33,9 +33,9 @@ void* HandleDebugManager(void* const server)
         return nullptr;
     }
 #if defined(IOS_PLATFORM) || defined(MAC_PLATFORM)
-    pthread_setname_np("OS_DbgConnThread");
+    pthread_setname_np("OS_DbgConThread");
 #else
-    pthread_setname_np(pthread_self(), "OS_DbgConnThread");
+    pthread_setname_np(pthread_self(), "OS_DbgConThread");
 #endif
 
     static_cast<ConnectServer*>(server)->RunServer();
