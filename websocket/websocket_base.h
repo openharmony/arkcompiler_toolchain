e4c64cf3b6b7acd6beb12e3b7f44d078fa209104
bigtea <tianqushen@huawei.com>
2024-07-04 19:49:52 +0800

    fix DebuggerClientTest & DebuggerCIntClientTest & WebSocketTest
    Signed-off-by: bigtea <tianqushen@huawei.com>

--- websocket/websocket_base.h
+++ websocket/websocket_base.h
@@ -99,9 +99,6 @@ protected:
 
     int connectionFd_ {-1};
 
-    // Used by testframe to skip sendCloseFrame
-    bool needSendCloseFrame_ {true};
-
     // Callbacks used during different stages of connection lifecycle.
     CloseConnectionCallback closeCb_;
     FailConnectionCallback failCb_;
