60d726f6eb2da449605e4dc152f6b40018d87761
Panferov Ivan <panferov.ivan@huawei-partners.com>
2025-06-19 21:34:15 +0800

    rename libpandafile in build system
    Issue: #ICFWS0
    
    Signed-off-by: Panferov Ivan <panferov.ivan@huawei-partners.com>

--- tooling/static/types/remote_object.cpp
+++ tooling/static/types/remote_object.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,7 +24,7 @@
 #include <utility>
 #include <variant>
 
-#include "libpandafile/helpers.h"
+#include "libarkfile/helpers.h"
 #include "macros.h"
 #include "utils/json_builder.h"
 
